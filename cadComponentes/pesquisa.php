244f286d5a46ed0c0ffa701de42fb82d367f787f
Gabrielescher <127734429+Gabrielescher@users.noreply.github.com>
2024-09-06 11:25:01 -0300

    Add files via upload

---
+++ cadComponentes/pesquisa.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Componentes - php comSQ MyL</title>
+</head>
+<body>
+    <div class="container">
+        <h4 class="text-primary text-center">Cadastro de componentes - Pesquisa de dados</h4>
+</div>
+</body>
+</html>
